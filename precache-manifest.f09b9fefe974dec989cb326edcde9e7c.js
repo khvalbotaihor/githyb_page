5c02d55cc48ffc79ab428ae850ebb7d345887461
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2020-08-30 01:02:31 +0300

    add new project

---
+++ precache-manifest.f09b9fefe974dec989cb326edcde9e7c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee2766f64e045aef24493731912c053c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c179938870b34bcc0953",
+    "url": "/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "dcdf1d66cb7e4141831f",
+    "url": "/static/js/2.114e66fc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.114e66fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c179938870b34bcc0953",
+    "url": "/static/js/main.2afde03f.chunk.js"
+  },
+  {
+    "revision": "cbb914445dea95ce6b65",
+    "url": "/static/js/runtime-main.a4a2ed45.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
